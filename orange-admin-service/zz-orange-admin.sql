3cb141d8d233827c9a574982706c227a7b546411
orange-form <63433734+orange-form@users.noreply.github.com>
2020-05-26 22:10:45 +0800

    Update zz-orange-admin.sql

--- orange-admin-service/zz-orange-admin.sql
+++ orange-admin-service/zz-orange-admin.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE zz-orange-admin DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
-USE zz-orange-admin
+CREATE DATABASE `zz-orange-admin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `zz-orange-admin`;
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
